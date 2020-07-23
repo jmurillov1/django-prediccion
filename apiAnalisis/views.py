a00638d1cef925c6d2cb302328c1a7cbdf892c09
Jordan Murillo <gatitojm.17@hotmail.com>
2020-07-23 17:46:30 -0500

    Arreglo Servicios

--- apiAnalisis/views.py
+++ apiAnalisis/views.py
@@ -160,11 +160,11 @@ class Clasificacion():
     def enviarDiagrama(request):
         try:
             hola=modeloAnalisis.crearGrafica(modeloAnalisis)
+            hola=";".join(hola)
             print(hola)
         except:
             hola="WRONG"
         return HttpResponse(hola)
-        #return render(request, "resultado.html",{"e":resul})
 
     def buscarCliente(request):
         try:
